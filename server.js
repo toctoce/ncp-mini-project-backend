348d91c9827a7d09c74845269c3bb2bd8eb3c286
toctoce <toctoce@naver.com>
2025-11-22 13:23:40 +0900

    fix: 세션 오류 수정

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: false, // https 적용 시 true로 변경 필요
-        maxAge: process.env.SESSION_TIME
+        maxAge: parseInt(process.env.SESSION_TIME)
     }
 }));
 
